c77d20fc9aef82f86554df94e284967a142880e1
jingming12 <153322875+jingming12@users.noreply.github.com>
2024-01-21 14:50:50 +0100

    Update baseline.sh

--- launch_scripts/baseline.sh
+++ launch_scripts/baseline.sh
@@ -4,6 +4,6 @@ target_domain=${1}
 --experiment=baseline \
 --experiment_name=baseline/${target_domain}/ \
 --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': '${target_domain}'}" \
---batch_size=128 \
+--batch_size=64 \
 --num_workers=5 \
 --grad_accum_steps=1
