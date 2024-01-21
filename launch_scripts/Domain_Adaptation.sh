6a24e3f43d0539387a7a1272bc73e221613b5122
jingming12 <153322875+jingming12@users.noreply.github.com>
2024-01-21 14:50:41 +0100

    Update Domain_Adaptation.sh

--- launch_scripts/Domain_Adaptation.sh
+++ launch_scripts/Domain_Adaptation.sh
@@ -4,6 +4,6 @@ python3 main.py \
 --experiment=Domain_Adaptation \
 --experiment_name=Domain_Adaptation/${target_domain}/ \
 --dataset_args="{'root': 'data/PACS', 'source_domain': 'art_painting', 'target_domain': '${target_domain}'}" \
---batch_size=128 \
+--batch_size=64 \
 --num_workers=5 \
 --grad_accum_steps=1
